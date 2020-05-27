4afbb08d8f25c266018da9ec0a30b4d6ae3cfd8a
miku86 <miku86coding@gmail.com>
2020-05-27 15:10:13 +0200

    Configure store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,3 +1,20 @@
-const store = {};
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import api from "../utils/api";
+import cardsReducer from "./cardsSlice";
+
+const store = configureStore({
+  reducer: {
+    cards: cardsReducer,
+  },
+  middleware: [
+    ...getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
+  ],
+});
+
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
