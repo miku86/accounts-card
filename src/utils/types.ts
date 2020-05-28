419056c1ece445c607c521b7d38208a678d913b4
miku86 <miku86coding@gmail.com>
2020-05-28 16:32:19 +0200

    Add placeholder and htmlid

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,6 +4,8 @@ export type Name = string;
 export type Url = string;
 export type Text = string;
 export type Icon = string;
+export type Placeholder = string;
+export type HTMLId = string;
 
 export interface Card {
   cardId: CardId;
@@ -19,6 +21,8 @@ export interface Account {
 export interface Platform {
   text: Text;
   icon: Icon;
+  placeholder: Placeholder;
+  id?: HTMLId;
 }
 
 export interface CardsState {
