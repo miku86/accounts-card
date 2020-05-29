772cc20ff2385f567ad7cd917632fb5c626648cc
miku86 <miku86coding@gmail.com>
2020-05-29 08:06:33 +0200

    Save user input in state

--- src/components/AccountsList.tsx
+++ src/components/AccountsList.tsx
@@ -1,7 +1,8 @@
 import List from "@material-ui/core/List";
 import { makeStyles, Theme } from "@material-ui/core/styles";
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { getPlatforms } from "../config/platforms";
+import { Account } from "../utils/types";
 import AccountsListItem from "./AccountsListItem";
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -14,11 +15,30 @@ interface Props {}
 
 export const AccountsList = (props: Props) => {
   const classes = useStyles();
+  const [accounts, setAccounts] = useState<Account[] | []>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newAccount = {
+      name: event.currentTarget.id,
+      url: event.currentTarget.value,
+    };
+
+    setAccounts((prevState: Account[] | []) => {
+      const withoutNewAccount = prevState.filter(
+        (prevAcc) => prevAcc.name !== newAccount.name
+      );
+      return [...withoutNewAccount, newAccount];
+    });
+  };
 
   return (
     <List className={classes.list}>
       {getPlatforms().map((platform) => (
-        <AccountsListItem key={platform.text} platform={platform} />
+        <AccountsListItem
+          key={platform.text}
+          platform={platform}
+          handleChange={handleChange}
+        />
       ))}
     </List>
   );
