80b7038e43b5a075853c5db8cb5c1472fa8e8a39
miku86 <miku86coding@gmail.com>
2020-05-29 18:13:06 +0200

    Add api method to create card

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,18 @@
-const api = {};
+import "firebase/auth";
+import { COLLECTIONS, db } from "../config/firebase";
+import { Card } from "./types";
+
+const api = {
+  createCard: async (card: Card) => {
+    if (card.accounts && card.accounts.length === 0) return;
+
+    try {
+      const doc = await db.collection(COLLECTIONS.CARDS).add(card);
+      return doc.id;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 
 export default api;
