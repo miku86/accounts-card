2d0a043888d31a5d0795d98fbb250486011c4859
miku86 <miku86coding@gmail.com>
2020-05-30 17:37:46 +0200

    Fix test errors

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,16 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import { Provider } from "react-redux";
 import { App } from "../components/App";
+import store from "../state/store";
 
 describe("App", () => {
   it("should render", () => {
-    const { container } = render(<App />);
+    const { container } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
     expect(container).toBeTruthy();
   });
 });
