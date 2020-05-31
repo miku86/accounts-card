05c7d14844417b48ebf49c656dcb566e8a13bcd7
miku86 <miku86coding@gmail.com>
2020-05-31 17:12:19 +0200

    Add routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,43 +1,22 @@
-import { Button } from "@material-ui/core";
-import { makeStyles, Theme } from "@material-ui/core/styles";
-import React, { useState } from "react";
-import AccountsList from "./AccountsList";
-import CreateCard from "./CreateCard";
-
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    minHeight: "100vh",
-    backgroundColor: "#EEE",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-}));
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CardDetails from "./CardDetails";
+import Home from "./Home";
 
 interface Props {}
 
-export const App = (props: Props) => {
-  const classes = useStyles();
-  const [open, setOpen] = useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
+export const App: React.FC = () => {
   return (
-    <div className={classes.root}>
-      <Button variant="contained" color="primary" onClick={handleClickOpen}>
-        Create Card
-      </Button>
-
-      <CreateCard open={open} handleClose={handleClose} />
-      <AccountsList />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/:id">
+          <CardDetails />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
