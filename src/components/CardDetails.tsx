fb8bd42c53d0a24b56ae3d54a58b9ff7391483ef
miku86 <miku86coding@gmail.com>
2020-06-01 19:13:14 +0200

    Add some styling

--- src/components/CardDetails.tsx
+++ src/components/CardDetails.tsx
@@ -1,8 +1,32 @@
-import React, { useEffect } from "react";
+import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import React, { createElement, useEffect } from "react";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
+import { findIcon } from "../config/platforms";
 import { fetchCard } from "../state/accountsSlice";
 import { Account, AppState, Card, CardId, Error } from "../utils/types";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    minHeight: "100vh",
+    backgroundColor: "#EEE",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  list: {
+    minWidth: "400px",
+  },
+  listItem: {
+    backgroundColor: theme.palette.background.paper,
+    margin: "10px 0",
+    borderRadius: "4px",
+    boxShadow:
+      "0 1px 1px 0 rgba(0,0,0,0.2),0 2px 1px -1px rgba(0,0,0,0.2),0 1px 3px 0 rgba(0,0,0,0.2)",
+  },
+}));
 
 interface Props {
   fetchCard: (cardId: CardId) => void;
@@ -11,27 +35,36 @@ interface Props {
 }
 
 const CardDetails = ({ fetchCard, fetchedCard, error }: Props) => {
+  const classes = useStyles();
   let { id: cardId } = useParams();
 
   useEffect(() => {
     fetchCard(cardId);
   }, [cardId, fetchCard]);
 
-  return error ? (
-    <div>{error}</div>
-  ) : fetchedCard ? (
-    <div>
-      <ul>
-        {fetchedCard.accounts &&
+  const createIcon = (platform: string) => createElement(findIcon(platform));
+
+  return (
+    <div className={classes.root}>
+      <List className={classes.list}>
+        {error ? (
+          <ListItem className={classes.listItem}>
+            <ListItemText primary={error} />
+          </ListItem>
+        ) : fetchedCard && fetchedCard.accounts ? (
           fetchedCard.accounts.map((account: Account) => (
-            <li>
-              {account.name} - {account.url}
-            </li>
-          ))}
-      </ul>
+            <ListItem className={classes.listItem}>
+              <ListItemIcon>{createIcon(account.name)}</ListItemIcon>
+              <ListItemText primary={account.url} secondary={account.name} />
+            </ListItem>
+          ))
+        ) : (
+          <ListItem className={classes.listItem}>
+            <ListItemText primary="Loading" />
+          </ListItem>
+        )}
+      </List>
     </div>
-  ) : (
-    <div>Loading</div>
   );
 };
 
