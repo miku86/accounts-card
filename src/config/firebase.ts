c78508c4838c50681d4646b1ad1760aa94246585
miku86 <git@miku86.com>
2020-09-09 14:19:04 +0200

    remove firebase analytics

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,4 +1,3 @@
-import "firebase/analytics";
 import * as firebase from "firebase/app";
 import "firebase/firestore";
 
@@ -19,6 +18,5 @@ export const COLLECTIONS = {
 
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
-firebase.analytics();
 
 export default firebase;
